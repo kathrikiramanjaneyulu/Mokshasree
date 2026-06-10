40a86eb16fc8cd236cfc73cf8a9bc641b6c0ad87
RAM <kathrikiramanjaneyulu@gmail.com>
2026-06-11 05:05:20 +0530

    updated new file

---
+++ src/test/java/GoogleSearch.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium Java");
+
+        searchBox.submit();
+
+        System.out.println("Search completed");
+
+        driver.quit();
+    }
+}
