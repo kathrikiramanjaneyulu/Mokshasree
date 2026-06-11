0ddc313cf54f11e9c60db5a75f4bbf91acfc9248
kathrikiramanjaneyulu <kathrikiramanjaneyulu@gmail.com>
2026-06-11 08:53:45 +0530

    Update GoogleSearch.java

--- src/test/java/GoogleSearch.java
+++ src/test/java/GoogleSearch.java
@@ -3,23 +3,47 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleSearch {
-    public static void main(String[] args) {
+public class YatraFlightBooking {
+
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
-        //ram
-        driver.get("https://www.google.com");
+
+        // Open Yatra
+        driver.get("https://www.yatra.com/");
         driver.manage().window().maximize();
 
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium Java");
+        Thread.sleep(3000);
 
-        searchBox.submit();
-        //kathrkiraman
-        
+        // Close login popup if displayed
+        try {
+            driver.findElement(By.xpath("//button[@aria-label='Close']")).click();
+        } catch (Exception e) {
+            System.out.println("Popup not displayed");
+        }
 
-        System.out.println("Search completed");
+        // Enter From City
+        WebElement fromCity = driver.findElement(By.xpath("//input[@placeholder='From']"));
+        fromCity.click();
+        fromCity.sendKeys("Bangalore");
 
+        Thread.sleep(2000);
+
+        // Enter To City
+        WebElement toCity = driver.findElement(By.xpath("//input[@placeholder='To']"));
+        toCity.click();
+        toCity.sendKeys("Delhi");
+
+        Thread.sleep(2000);
+
+        // Click Search Button
+        driver.findElement(By.xpath("//button[contains(text(),'Search')]")).click();
+
+        Thread.sleep(5000);
+
+        System.out.println("Flight search completed successfully");
+
+        // Do not proceed to payment
         driver.quit();
     }
 }
